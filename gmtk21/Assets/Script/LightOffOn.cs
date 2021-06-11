fd0a0b5c9fb26e48856f33fdf2de2c48775d8c2d
IchHalt <firen8@hotmail.de>
2021-06-11 23:58:09 +0200

    Light Off

---
+++ gmtk21/Assets/Script/LightOffOn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOffOn : MonoBehaviour
+{
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LightOff());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator LightOff()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        GetComponent<Light>().enabled = false;
+
+    }
+}
