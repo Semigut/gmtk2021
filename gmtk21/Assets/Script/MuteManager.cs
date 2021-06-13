57a2608669c46d868f753d692e77ec32fa7ee08f
IchHalt <firen8@hotmail.de>
2021-06-13 12:04:40 +0200

    Background+ Mute etc

--- gmtk21/Assets/Script/MuteManager.cs
+++ gmtk21/Assets/Script/MuteManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MuteManager : MonoBehaviour
 {
 
     private bool isMuted;
+    public Text MutedOnOff;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,22 @@ public class MuteManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isMuted)
+        {
+
+            MutedOnOff.text = "Sound: Off";
+        }
+        else
+        {
+            MutedOnOff.text = "Sound: On";
+        }
+    }
+
+    public void MutePressed()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
         
     }
+
 }
