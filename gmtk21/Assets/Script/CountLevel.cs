c8c598268cbf4180e0a807d93fc02874e205a2c0
IchHalt <firen8@hotmail.de>
2021-06-13 10:43:54 +0200

    Sound + Level Count

---
+++ gmtk21/Assets/Script/CountLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountLevel : MonoBehaviour
+{
+
+    public Text LevelCount;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelCount.text = "Level " + SceneManager.GetActiveScene().buildIndex.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ gmtk21/Assets/Script/MuteManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteManager : MonoBehaviour
+{
+
+    private bool isMuted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMuted = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
